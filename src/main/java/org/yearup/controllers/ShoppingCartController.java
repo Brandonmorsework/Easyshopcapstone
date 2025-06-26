a56c0cc6f43f54d688b59411bf5b146ca3a18ea3
Brandonmorsework <brandonmorsework@gmail.com>
2025-06-26 10:34:28 -0400

    created QuantityDto class

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,5 +1,6 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +18,8 @@ import java.security.Principal;
 // convert this class to a REST controller
 @RestController
 @RequestMapping("cart")
-
 // only logged-in users should have access to these actions
+@PreAuthorize("isAuthenticated()")
 public class ShoppingCartController
 {
     // a shopping cart requires
@@ -26,8 +27,14 @@ public class ShoppingCartController
     private UserDao userDao;
     private ProductDao productDao;
 
+    @Autowired
+    public ShoppingCartController(ShoppingCartDao shoppingCartDao, UserDao userDao, ProductDao productDao) {
+        this.shoppingCartDao = shoppingCartDao;
+        this.userDao = userDao;
+        this.productDao = productDao;
+    }
 
-
+    @GetMapping
     // each method in this controller requires a Principal object as a parameter
     public ShoppingCart getCart(Principal principal)
     {
@@ -51,7 +58,7 @@ public class ShoppingCartController
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
     @PostMapping("/products/{productId}")
-    @PreAuthorize("permitAll()")
+    @PreAuthorize("isAuthenticated()")
     @ResponseStatus(value = HttpStatus.CREATED)
     public ShoppingCart addToUserCart(@PathVariable int productId, Principal principal)
     {
@@ -73,7 +80,8 @@ public class ShoppingCartController
     // https://localhost:8080/cart/products/15 (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
     @PutMapping("/products/{productId}")
-    @PreAuthorize("permitAll()")
+    @PreAuthorize("isAuthenticated()")
+
     public void updateCart(@PathVariable int productId, @RequestBody int quantity, Principal principal)
     {
         try
---
+++ src/main/java/org/yearup/models/authentication/QuantityDto.java
@@ -0,0 +1,13 @@
+package org.yearup.models.authentication;
+
+public class QuantityDto {
+    private int quantity;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
